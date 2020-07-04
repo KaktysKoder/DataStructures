43d0e55b0d33b249c98288731fa31d145ab071ef
Rimuru <kaktysKoder@gmail.com>
2020-07-04 08:03:47 +0400

    Реализовал стек тремя разными способами. Далее осталось протестировать на исключительные ситуации, и провести рефакторинг кода.

---
+++ SLL/Stack/Model/ArrayStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Stack.Model
+{
+    internal sealed class ArrayStack<T>
+    {
+        private readonly T[] items;
+        private readonly int size = 10;
+        private int current = -1;
+
+        public ArrayStack(int size = 10)
+        {
+            items = new T[size];
+            this.size = size;
+        }
+
+        public ArrayStack(T data, int size = 10) : this(size)
+        {
+            items[0] = data;
+            current  = 1;
+        }
+
+        public int Count => items.Length;
+
+        public void Push(T data)
+        {
+            if (current < size - 1)
+            {
+                current++;
+                items[current] = data;
+            }
+            else
+            {
+                throw new StackOverflowException("Переполнение стека");
+            }
+        }
+        public T Pop()
+        {
+            if (current >= 0)
+            {
+                var item = items[current];
+                items[current] = default;
+                current--;
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Стек пуст");
+            }
+        }
+
+        public T Peek()
+        {
+            if (current >= 0)
+            {
+                return items[current];
+            }
+            else
+            {
+                throw new NullReferenceException("Стек пуст");
+            }
+        }
+    }
+}
--- SLL/Stack/StartProgram.cs
+++ SLL/Stack/StartProgram.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Stack.Model;
+using System;
 
 namespace Stack
 {
-    class StartProgram
+    internal sealed class StartProgram
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello Stack!");
+            Test1();
+        }
+        private static void Test1()
+        {
+            //var stack = new StackList<int>();
+
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+
+            //var item1 = stack.Pop();
+            //var item2 = stack.Peek();
+
+            //Console.WriteLine(item1);
+            //Console.WriteLine(item2);
+
+            //
+
+            //var linkedStack = new LinkedStack<int>();
+
+            //linkedStack.Push(10);
+            //linkedStack.Push(20);
+            //linkedStack.Push(30);
+            //linkedStack.Push(40);
+            //linkedStack.Push(50);
+
+            //Console.WriteLine(linkedStack.Peek());
+            //Console.WriteLine(linkedStack.Pop());
+            //Console.WriteLine(linkedStack.Pop());
+            //Console.WriteLine(linkedStack.Peek());
+
+            ArrayStack<int> array = new ArrayStack<int>(5);
+
+            array.Push(100);
+            array.Push(200);
+            array.Push(300);
+            array.Push(400);
+            array.Push(500);
+
+            //Ex
+            //array.Push(500);
+
+            Console.WriteLine(array.Peek());
+            Console.WriteLine(array.Pop());
+            Console.WriteLine(array.Pop());
+            Console.WriteLine(array.Pop());
+            Console.WriteLine(array.Pop());
+            Console.WriteLine(array.Pop());
+            //Ex
+            //Console.WriteLine(array.Pop());
+        }
+
+        private static void FunctionalityCheck()
+        {
+
         }
     }
 }
