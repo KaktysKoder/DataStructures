ca6f2f00e3dfbd1f7c5e0c62b7665b890847b329
Rimuru <kaktysKoder@gmail.com>
2020-07-04 06:44:39 +0400

    init ...

---
+++ SLL/Stack/StartProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack
+{
+    class StartProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Stack!");
+        }
+    }
+}
