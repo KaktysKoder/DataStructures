9bbab8fd6c6f4b43437e9ef7f8f5778a31a3be68
Rimuru <kaktysKoder@gmail.com>
2020-07-05 13:29:21 +0400

    Реализовал очередь используя одномерный массив

--- Queue/Queue/StartProgram.cs
+++ Queue/Queue/StartProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Queue.Model;
+using Queue.Model.ArrayQueue;
 
 namespace Queue
 {
@@ -6,7 +8,38 @@ namespace Queue
     {
         private static void Main()
         {
-            Console.WriteLine("Hello Queue!");
+            TestArrayQueue();
+        }
+
+        private static void TestArrayQueue()
+        {
+            ArrayQueue<int> array = new ArrayQueue<int>(5);
+
+            array.Enqueue(10);
+            array.Enqueue(20);
+            array.Enqueue(30);
+            array.Enqueue(40);
+            array.Enqueue(50);
+
+            Console.WriteLine(array.Dnqueue());
+            Console.WriteLine(array.Peek());
+            Console.WriteLine(array.Dnqueue());
+        }
+
+        private static void TestLLQueue()
+        {
+            var easyQueue = new SheetWrapperForQueue<int>();
+
+            easyQueue.Enqueue(10);
+            easyQueue.Enqueue(20);
+            easyQueue.Enqueue(30);
+            easyQueue.Enqueue(40);
+            easyQueue.Enqueue(50);
+
+
+            Console.WriteLine(easyQueue.Dnqueue());
+            Console.WriteLine(easyQueue.Peek());
+            Console.WriteLine(easyQueue.Dnqueue());
         }
     }
 }
