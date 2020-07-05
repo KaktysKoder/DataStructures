d5a6de5fd0b48ba27f5e5e73e91e19d69945ffc9
Rimuru <kaktysKoder@gmail.com>
2020-07-05 12:52:54 +0400

    Create class SheetWrapperForQueue<T> Очередь на основе связного списка (обёртка) над списком.

---
+++ Queue/Queue/Model/EasyQueue/SheetWrapperForQueue.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Queue.Model
+{
+    internal sealed class SheetWrapperForQueue<T>
+    {
+        private List<T> items = new List<T>();
+
+        public SheetWrapperForQueue() { }
+
+        public SheetWrapperForQueue(T data) => items.Add(data);
+
+        private T Head   => items.Last();
+        private T Tail   => items.First();
+        public int Count => items.Count;
+
+
+        public void Enqueue(T data) => items.Insert(0, data);
+
+        public T Dnqueue()
+        {
+            var item = Head;
+
+            items.Remove(item);
+
+            return item;
+        }
+
+        public T Peek() => Head;
+
+        public void Clear() => items.Clear();
+    }
+}
