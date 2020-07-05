b6a0561a35c7fe50e23ae0424464ce2721628fb7
Rimuru <kaktysKoder@gmail.com>
2020-07-05 12:08:55 +0400

    init queue

---
+++ Queue/Queue/StartProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Queue
+{
+    internal sealed class StartProgram
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello Queue!");
+        }
+    }
+}
